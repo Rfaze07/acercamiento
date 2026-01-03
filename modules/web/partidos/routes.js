c18d2e761e44a6142f81b77c8deea98d07ceb22e
Rfaze07 <renzofazele@gmail.com>
2026-01-03 15:25:17 -0300

    estadisticassss

--- modules/web/partidos/routes.js
+++ modules/web/partidos/routes.js
@@ -6,5 +6,8 @@ const { auth, checkAcceso } = require('../../../middlewares');
 
 router.get('/web/partidos', controller.getLista);
 router.post('/web/partidos/getLista', controller.getListaAjax);
+router.get('/web/partidos/detalle', controller.getListaDetalles);
+router.get('/web/detallePartidos', controller.getListaDetalles); // Alias para la ruta solicitada
+router.post('/web/partidos/getListaDetallesAjax', controller.getListaDetallesAjax);
 
 module.exports = router
